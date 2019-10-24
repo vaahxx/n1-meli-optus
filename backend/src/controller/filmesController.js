6f0564a13a4ec1ce178b8537711acd4e36e3f2f2
valentina <vaahxx@gmail.com>
2019-10-23 22:14:08 -0300

    rotas getFilmes e getFilmesById prontas e funcionando

--- backend/src/controller/filmesController.js
+++ backend/src/controller/filmesController.js
@@ -2,4 +2,10 @@ const filmesJson = require('../model/filmes');
 
 exports.getFilmes = (request, response) => {
     response.status(200).send(filmesJson);
+};
+
+exports.getFilmeById = (request, response) => {
+    const idUrl = request.params.id;
+    const filme = filmesJson.find(e => e.id = idUrl);
+    response.status(200).send(filme);
 }
